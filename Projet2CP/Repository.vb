2ece7a54d8ab1332b38c0b2987e6cfda31a67729
nassim199 <hammadounassim0@gmail.com>
2020-04-11 22:19:15 +0100

    .gitignore file added

--- Projet2CP/Repository.vb
+++ Projet2CP/Repository.vb
@@ -5,7 +5,7 @@
     Public Shared Sub initialiser()
         'initialiser la connexion avec la bdd
         Dim dbConnString As String
-        Dim path As String = "E:\ESI\Projet2CP\db.accdb"
+        Dim path As String = "C:\Users\user\Documents\Programmation\Visual Basic\Excel\bdd.accdb"
         dbConnString = "provider=microsoft.ace.oledb.12.0;data source=" & path
 
         _connection.ConnectionString = dbConnString
