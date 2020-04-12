0adc3dad11a619a62e3e1de49c78e7abf865926f
mounir2KH <it_khodja@esi.dz>
2020-04-12 23:55:57 +0100

    RechercheEtudiant view fixed

--- Projet2CP/Repository.vb
+++ Projet2CP/Repository.vb
@@ -5,7 +5,7 @@
     Public Shared Sub initialiser()
         'initialiser la connexion avec la bdd
         Dim dbConnString As String
-        Dim path As String = "SPECIFY PATH HERE TO MAKE THE CODE RUN"
+        Dim path As String = "C:/Users/dell/Desktop/db.accdb"
         dbConnString = "provider=microsoft.ace.oledb.12.0;data source=" & path
 
         _connection.ConnectionString = dbConnString
