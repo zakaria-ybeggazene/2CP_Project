b0750c4a613459522af6cea87c887d3bab743b86
Haroun <inoone707@gmail.com>
2020-05-03 16:31:34 +0100

    Modification MVVM

---
+++ 2CP_Project/Projet2CP/View/EtudiantView.xaml.vb
@@ -0,0 +1,16 @@
+﻿Public Class EtudiantView
+    Private Sub Sexecb_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Dim list As New List(Of String)
+        list.Add("Masculin")
+        list.Add("Féminin")
+        Sexecb.ItemsSource = list
+    End Sub
+    Private Sub ComboBox_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+        Dim list As New List(Of String)
+        list.Add("1")
+        list.Add("2")
+        list.Add("3")
+        list.Add("4")
+        NiveauCB.ItemsSource = list
+    End Sub
+End Class
